5afb5cc78df14a17928f351bbf55e7934ad0624c
dhikra909 <alm@ka909@gmail.com>
2023-01-16 04:58:05 -0800

    Read, Update

---
+++ src/main/java/api2.java
@@ -0,0 +1,140 @@
+
+public class api2 {
+
+	private	String cca2 ;
+	private	String ccn3;
+	private	String occa3;
+	private	String cioc ;
+	private	String status;
+	private	String region ;
+	private	String subregion;
+	private	String flag ;
+	private	String fifa;
+	private	String startofweek;
+	
+	private boolean independent;
+	public boolean isIndependent() {
+		return independent;
+	}
+	public void setIndependent(boolean independent) {
+		this.independent = independent;
+	}
+	public boolean isUnMember() {
+		return unMember;
+	}
+	public void setUnMember(boolean unMember) {
+		this.unMember = unMember;
+	}
+	public boolean isLandlocked() {
+		return landlocked;
+	}
+	public void setLandlocked(boolean landlocked) {
+		this.landlocked = landlocked;
+	}
+	public float getAra() {
+		return ara;
+	}
+	public void setAra(float ara) {
+		this.ara = ara;
+	}
+	public Integer getPopulation() {
+		return population;
+	}
+	public void setPopulation(Integer population) {
+		this.population = population;
+	}
+	private boolean unMember;
+	private boolean landlocked;
+	
+	private float ara;
+	private Integer population;
+	
+	
+	
+	
+	
+	
+	public String getCca2() {
+		return cca2;
+	}
+	public void setCca2(String cca2) {
+		this.cca2 = cca2;
+	}
+	public String getCcn3() {
+		return ccn3;
+	}
+	public void setCcn3(String ccn3) {
+		this.ccn3 = ccn3;
+	}
+	public String getOcca3() {
+		return occa3;
+	}
+	public void setOcca3(String occa3) {
+		this.occa3 = occa3;
+	}
+	public String getCioc() {
+		return cioc;
+	}
+	public void setCioc(String cioc) {
+		this.cioc = cioc;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getSubregion() {
+		return subregion;
+	}
+	public void setSubregion(String subregion) {
+		this.subregion = subregion;
+	}
+	public String getFlag() {
+		return flag;
+	}
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+	public String getFifa() {
+		return fifa;
+	}
+	public void setFifa(String fifa) {
+		this.fifa = fifa;
+	}
+	public String getStartofweek() {
+		return startofweek;
+	}
+	public void setStartofweek(String startofweek) {
+		this.startofweek = startofweek;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
